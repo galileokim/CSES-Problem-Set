6c8a9ab4d027f26fb38e0946c9d4c7edfff238ce
galileokim <galileokim451@gmail.com>
2024-02-19 16:47:05 -0700

    removed unnecessary stuff

--- Search and Sort/restaruant.cpp
+++ Search and Sort/restaruant.cpp
@@ -8,8 +8,8 @@ int main() {
 	for(int i=0;i<n;i++) {
 		int a,b;
 		cin >> a >> b;
-		s.insert({2*a,1});
-		s.insert({2*b+1,-1});
+		s.insert({a,1});
+		s.insert({b,-1});
 	}
 	int ans = 0, c=0;
 	for(array<int,2> a:s) {
