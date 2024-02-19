9973ba08093a925f612644928c33ef56059f9063
galileokim <galileokim451@gmail.com>
2024-02-19 16:24:33 -0700

    .

---
+++ Search and Sort/restaruant.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	set<array<int,2>> s;
+	for(int i=0;i<n;i++) {
+		int a,b;
+		cin >> a >> b;
+		s.insert({2*a,1});
+		s.insert({2*b+1,-1});
+	}
+	int ans = 0, c=0;
+	for(array<int,2> a:s) {
+		c+=a[1];
+		ans=max(c,ans);
+	}
+	cout << ans;
+}
